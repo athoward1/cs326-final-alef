689c0f181f2111f3602b68467d89da9731a8cfd0
athoward1 <and1howard@verizon.net>
2020-11-08 23:14:38 -0500

    added a login/sign up form to homepage, no functionality yet

--- homepage.js
+++ homepage.js
@@ -1,5 +1,6 @@
 "use strict";
 window.addEventListener("load", async function() {
+    let isOpen = true;
     document.getElementById('addButton').addEventListener('click',()=>{
         //add another workspace box in the first position and move every other box over one
         
@@ -10,34 +11,44 @@ window.addEventListener("load", async function() {
 
         let deleteBox = document.createElement("img");
         deleteBox.src = "https://cdn3.iconfinder.com/data/icons/ui-essential-elements-buttons/110/DeleteDustbin-512.png"
-        deleteBox.style.width = "50px"
-        deleteBox.style.height = "50px"
+        deleteBox.style.width = "50px";
+        deleteBox.style.height = "50px";
+        deleteBox.className = "cursorPointer";  
 
         let editBox = document.createElement("img");
         editBox.src = "https://image.flaticon.com/icons/png/512/84/84380.png"
-        editBox.style.width = "20px"
-        editBox.style.height = "20px"
+        editBox.style.width = "20px";
+        editBox.style.height = "20px";
+        editBox.className = "cursorPointer";
         
         let boxName = document.createElement("span");
         boxName.innerHTML = "New Box";
         boxName.className = "workspaceNameText";
+        boxName.style.fontWeight = "bold";
+
         deleteBox.addEventListener("click", ()=> {
             row1.removeChild(addBox);
         });
         editBox.addEventListener("click", () =>{
-            let newName = document.createElement("input");
-            let saveName = document.createElement("button");
-            saveName.className = "btn btn-primary";
-            saveName.innerHTML = "Save Name"
-            addBox.appendChild(newName);
-            addBox.appendChild(saveName);
-            saveName.addEventListener("click", ()=>{
-                boxName.innerHTML = newName.value;
-                addBox.appendChild(boxName);
-                addBox.removeChild(newName);
-                addBox.removeChild(saveName);
-                addBox.appendChild(editBox);
-            });
+            
+            if(isOpen){
+                let newName = document.createElement("input");
+                let saveName = document.createElement("button");
+                saveName.className = "btn btn-primary";
+                saveName.innerHTML = "Save Title";
+                newName.placeholder = "Enter New Title";
+                addBox.appendChild(newName);
+                addBox.appendChild(saveName);
+                saveName.addEventListener("click", ()=>{
+                    boxName.innerHTML = newName.value;
+                    addBox.appendChild(boxName);
+                    addBox.removeChild(newName);
+                    addBox.removeChild(saveName);
+                    addBox.appendChild(editBox);
+                    isOpen = true;
+                });
+                isOpen = false;
+            }
         });
         
         addBox.appendChild(deleteBox);
@@ -46,5 +57,6 @@ window.addEventListener("load", async function() {
         
 
     });
+    
     
 });
