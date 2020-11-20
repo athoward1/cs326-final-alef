22e3caf818e0a162414ee7cdcf94760830b147b6
Olin <ogoudey@umass.edu>
2020-11-19 19:30:16 -0500

    Setting page update and Change PAssword functionality3

--- server/index.js
+++ server/index.js
@@ -114,7 +114,7 @@ app.post("/createAccount", findUser, createAccount);
 
 app.post("/login", checkPassword);
 
-async function deleteAccount(req,res){
+async function deleteAccount(req,res, next){
     await connectAndRun(db => db.none("DELETE * FROM logins WHERE userid = ($1);", [req.body.username]));   //there better be exactly one
     next();
     
